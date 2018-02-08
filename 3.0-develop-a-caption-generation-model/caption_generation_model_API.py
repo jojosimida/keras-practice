bca59810adcaf239352566838cde810aa9f0a60a
jojosimida <ms0421555@gmail.com>
2018-02-08 12:19:03 +0800

    Add files via upload

---
+++ 3.0-develop-a-caption-generation-model/caption_generation_model_API.py
@@ -0,0 +1,361 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+from os import listdir
+import numpy as np
+import pandas as pd
+import string
+from nltk.translate.bleu_score import corpus_bleu
+from pickle import load, dump
+from IPython.display import Image
+ 
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils import plot_model, to_categorical
+from keras.preprocessing.image import load_img, img_to_array
+from keras.applications.vgg16 import VGG16, preprocess_input 
+from keras.models import Model
+from keras.layers import Input, Dense, Flatten, LSTM
+from keras.layers import RepeatVector, TimeDistributed
+from keras.layers import Embedding
+from keras.layers.merge import concatenate
+from keras.layers.pooling import GlobalMaxPooling2D
+
+
+# In[2]:
+
+
+# load doc into memory
+def load_doc(filename):
+    # open the file as read only
+    file = open(filename, 'r')
+    # read all text
+    text = file.read()
+    # close the file
+    file.close()
+    return text
+
+
+# In[3]:
+
+
+# extract descriptions for images
+def load_descriptions(doc):
+    mapping = dict()
+    # process lines
+    for line in doc.split('\n'):
+        # split line by white space
+        tokens = line.split()
+        if len(line) < 2:
+            continue
+        # take the first token as the image id, the rest as the description
+        image_id, image_desc = tokens[0], tokens[1:]
+        # remove filename from image id
+        image_id = image_id.split('.')[0]
+        # convert description tokens back to string
+        image_desc = ' '.join(image_desc)
+        # store the first description for each image
+        if image_id not in mapping:
+            mapping[image_id] = image_desc
+    return mapping
+
+
+# In[4]:
+
+
+def clean_descriptions(descriptions):
+    """
+    This uses the 3-argument version of str.maketrans
+    with arguments (x, y, z) where 'x' and 'y'
+    must be equal-length strings and characters in 'x'
+    are replaced by characters in 'y'. 'z'
+    is a string (string.punctuation here)
+    where each character in the string is mapped
+    to None.
+    
+    This is an alternative that creates a dictionary mapping
+    of every character from string.punctuation to None (this will
+    also work)
+    #translator = str.maketrans(dict.fromkeys(string.punctuation))
+    """
+    # prepare translation table for removing punctuation
+    table = str.maketrans('', '', string.punctuation)
+    for key, desc in descriptions.items():
+        # tokenize
+        desc = desc.split()
+        # convert to lower case
+        desc = [word.lower() for word in desc]
+        # remove punctuation from each token
+        desc = [w.translate(table) for w in desc]
+        # remove hanging 's' and 'a'
+        desc = [word for word in desc if len(word)>1]
+        # store as string
+        descriptions[key] =  ' '.join(desc)
+
+
+# In[5]:
+
+
+# save descriptions to file, one per line
+def save_doc(descriptions, filename):
+    lines = list()
+    for key, desc in descriptions.items():
+        lines.append(key + ' ' + desc)
+    data = '\n'.join(lines)
+    file = open(filename, 'w')
+    file.write(data)
+    file.close()
+
+
+# In[6]:
+
+
+# extract features from each photo in the directory
+def extract_features(directory):
+    # load the model
+    in_layer = Input(shape=(224, 224, 3))
+    model = VGG16(include_top=False, input_tensor=in_layer)
+    print(model.summary())
+    # extract features from each photo
+    features = dict()
+    for name in listdir(directory):
+        # load an image from file
+        filename = directory + '/' + name
+        image = load_img(filename, target_size=(224, 224))
+        # convert the image pixels to a numpy array
+        image = img_to_array(image)
+        # reshape data for the model
+        image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+        # prepare the image for the VGG model
+        image = preprocess_input(image)
+        # get features
+        feature = model.predict(image, verbose=0)
+        # get image id
+        image_id = name.split('.')[0]
+        # store feature
+        features[image_id] = feature
+        
+        if len(features)%100==0:
+            print('>{}'.format(name))
+    return features
+
+
+# In[7]:
+
+
+# load a pre-defined list of photo identifiers
+def load_set(filename):
+    doc = load_doc(filename)
+    dataset = list()
+    # process line by line
+    for line in doc.split('\n'):
+        # skip empty lines
+        if len(line) < 1:
+            continue
+        # get the image identifier
+        identifier = line.split('.')[0]
+        dataset.append(identifier)
+    return set(dataset)
+
+
+# In[8]:
+
+
+# split a dataset into train/test elements
+def train_test_split(dataset):
+    # order keys so the split is consistent
+    ordered = sorted(dataset)
+    # return split dataset as two new sets
+    return set(ordered[:100]), set(ordered[100:200])
+
+
+# In[9]:
+
+
+# load clean descriptions into memory
+def load_clean_descriptions(filename, dataset):
+    # load document
+    doc = load_doc(filename)
+    descriptions = dict()
+    for line in doc.split('\n'):
+        # split line by white space
+        tokens = line.split()
+        # split id from description
+        image_id, image_desc = tokens[0], tokens[1:]
+        # skip images not in the set
+        if image_id in dataset:
+            # store
+            descriptions[image_id] = 'startseq ' + ' '.join(image_desc) + ' endseq'
+    return descriptions
+
+
+# In[10]:
+
+
+# load photo features
+def load_photo_features(filename, dataset):
+    # load all features
+    all_features = load(open(filename, 'rb'))
+    # filter features
+    features = {k: all_features[k] for k in dataset}
+    return features
+
+
+# In[11]:
+
+
+# fit a tokenizer given caption descriptions
+def create_tokenizer(descriptions):
+    lines = list(descriptions.values())
+    tokenizer = Tokenizer()
+    tokenizer.fit_on_texts(lines)
+    return tokenizer
+
+
+# In[12]:
+
+
+# create sequences of images, input sequences and output words for an image
+def create_sequences(tokenizer, desc, image, max_length):
+    Ximages, XSeq, y = list(), list(),list()
+    vocab_size = len(tokenizer.word_index) + 1
+    
+    """
+    ex:
+    [desc] = ['man in black is sitting next to modern art structure in front of glass building']
+    tokenizer.texts_to_sequences([desc]) -> 
+              [[8, 1, 10, 5, 48, 77, 16, 2354, 873, 375, 1, 39, 7, 584, 127]]
+    """
+    # integer encode the description
+    seq = tokenizer.texts_to_sequences([desc])[0]
+    
+    # split one sequence into multiple X,y pairs
+    for i in range(1, len(seq)):
+        # select
+        in_seq, out_seq = seq[:i], seq[i]
+        # pad input sequence
+        in_seq = pad_sequences([in_seq], maxlen=max_length)[0]
+        # encode output sequence
+        out_seq = to_categorical([out_seq], num_classes=vocab_size)[0]
+        # store
+        Ximages.append(image)
+        XSeq.append(in_seq)
+        y.append(out_seq)
+    # Ximages, XSeq, y = array(Ximages), array(XSeq), array(y)
+    return [Ximages, XSeq, y]
+
+
+# In[13]:
+
+
+# define the captioning model
+def define_model(vocab_size, max_length):
+    # feature extractor (encoder)
+    inputs1 = Input(shape=(7, 7, 512))
+    fe1 = GlobalMaxPooling2D()(inputs1)
+    fe2 = Dense(128, activation='relu')(fe1)
+    fe3 = RepeatVector(max_length)(fe2)
+    # embedding
+    inputs2 = Input(shape=(max_length,))
+    emb2 = Embedding(vocab_size, 50, mask_zero=True)(inputs2)
+    emb3 = LSTM(256, return_sequences=True)(emb2)
+    emb4 = TimeDistributed(Dense(128, activation='relu'))(emb3)
+    # merge inputs
+    merged = concatenate([fe3, emb4])
+    # language model (decoder)
+    lm2 = LSTM(500)(merged)
+    lm3 = Dense(500, activation='relu')(lm2)
+    outputs = Dense(vocab_size, activation='softmax')(lm3)
+    # tie it together [image, seq] [word]
+    model = Model(inputs=[inputs1, inputs2], outputs=outputs)
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    print(model.summary())
+    plot_model(model, show_shapes=True, to_file='plot.png')
+
+    return model
+
+
+# In[14]:
+
+
+# data generator, intended to be used in a call to model.fit_generator()
+def data_generator(descriptions, features, tokenizer, max_length, n_step):
+    # loop until we finish training
+    while 1:
+        # loop over photo identifiers in the dataset
+        keys = list(descriptions.keys())
+        for i in range(0, len(keys), n_step):
+            Ximages, XSeq, y = list(), list(),list()
+            for j in range(i, min(len(keys), i+n_step)):
+                image_id = keys[j]
+                # retrieve photo feature input
+                image = features[image_id][0]
+                # retrieve text input
+                desc = descriptions[image_id]
+                # generate input-output pairs
+                in_img, in_seq, out_word = create_sequences(tokenizer, desc, image, max_length)
+                for k in range(len(in_img)):
+                    Ximages.append(in_img[k])
+                    XSeq.append(in_seq[k])
+                    y.append(out_word[k])
+            # yield this batch of samples to the model
+            yield [[np.array(Ximages), np.array(XSeq)], np.array(y)]
+
+
+# In[15]:
+
+
+# map an integer to a word
+def word_for_id(integer, tokenizer):
+    for word, index in tokenizer.word_index.items():
+        if index == integer:
+            return word
+    return None
+
+# generate a description for an image
+def generate_desc(model, tokenizer, photo, max_length):
+    # seed the generation process
+    in_text = 'startseq'
+    # iterate over the whole length of the sequence
+    for i in range(max_length):
+        # integer encode input sequence
+        sequence = tokenizer.texts_to_sequences([in_text])[0]
+        # pad input
+        sequence = pad_sequences([sequence], maxlen=max_length)
+        # predict next word
+        yhat = model.predict([photo,sequence], verbose=0)
+        # convert probability to integer
+        yhat = np.argmax(yhat)
+        # map integer to word
+        word = word_for_id(yhat, tokenizer)
+        # stop if we cannot map the word
+        if word is None:
+            break
+        # append as input for generating the next word
+        in_text += ' ' + word
+        # stop if we predict the end of the sequence
+        if word == 'endseq':
+            break
+    return in_text
+
+
+# In[16]:
+
+
+# evaluate the skill of the model
+def evaluate_model(model, descriptions, photos, tokenizer, max_length):
+    actual, predicted = list(), list()
+    # step over the whole set
+    for key, desc in descriptions.items():
+        # generate description
+        yhat = generate_desc(model, tokenizer, photos[key], max_length)
+        # store actual and predicted
+        actual.append([desc.split()])
+        predicted.append(yhat.split())
+    # calculate BLEU score
+    bleu = corpus_bleu(actual, predicted)
+    return bleu
+
